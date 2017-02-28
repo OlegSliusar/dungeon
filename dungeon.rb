3df9d420be191a35de27844e4233f375816fedb3
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-28 14:51:08 +0200

    Create initial classes

--- dungeon.rb
+++ dungeon.rb
@@ -14,3 +14,14 @@ class Player
     @name = player_name
   end
 end
+
+class Room
+  attr_accessor :reference, :name, :description, :connections
+
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+end
